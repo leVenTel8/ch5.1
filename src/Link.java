6c849b727709c2d0e30dabe9c6c27739f69689cc
Vitaly Frolov <leventel8@yandex.ru>
2017-02-24 19:42:50 +0400

    задание 5.1

---
+++ src/Link.java
@@ -0,0 +1,16 @@
+class Link {
+
+    public long dData;
+    public Link next;
+
+    public  Link(long dd){
+
+        dData = dd;
+
+    }
+
+    public void displayLink(){
+        System.out.print(dData + " ");
+    }
+
+}
